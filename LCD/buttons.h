5359b471ef7c3d1b1e9ee99537946705fb72d945
Kamil Gierlach <48731610+Gierlaszek@users.noreply.github.com>
2021-01-12 17:55:45 +0100

    Add a button handler, correct small changes

--- LCD/buttons.h
+++ LCD/buttons.h
@@ -6,6 +6,12 @@
 
 void PORTB_IRQHandler(void);
 void buttonsInitialize(void);
+uint8_t name_button(void);
+void set_v_name(uint8_t i);
+uint8_t correct_button(void);
+void set_v_correct(uint8_t i);
+uint8_t incorrect_button(void);
+void set_v_incorrect(uint8_t i);
 
 #endif
 
--- LCD/font.h
+++ LCD/font.h
@@ -4,5 +4,5 @@
 #include <stdint.h>
  
 extern const uint8_t font_ASCII[][5];
- 
+
 #endif /* FONT_H_ */
