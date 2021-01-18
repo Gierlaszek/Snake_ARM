76c48bc581508663a960b6ba025487144aa67461
Kamil Gierlach <48731610+Gierlaszek@users.noreply.github.com>
2021-01-18 19:56:21 +0100

    Delete file LCD from main branch

--- LCD/bitmap.h
+++
@@ -1,8 +0,0 @@
-#ifndef BITMAP_H
-#define BITMAP_H
-
-#include <stdint.h>
-
-extern const uint8_t logo[];
-
-#endif
--- LCD/font.h
+++
@@ -1,8 +0,0 @@
-#ifndef FONT_H_
-#define FONT_H_
- 
-#include <stdint.h>
- 
-extern const uint8_t font_ASCII[][5];
-
-#endif /* FONT_H_ */
